39c90ea7f7f99359ce614026db92e9b51974a962
zjyezaire <zaire.elleby@gmail.com>
2024-09-26 11:32:41 -0400

    added ejs file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", async (req, res) => {
+  res.render(index.ejs);
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
