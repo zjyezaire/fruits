e398f2b7f6085d7197d9da24f7c595dfabaea545
zjyezaire <zaire.elleby@gmail.com>
2024-09-26 16:25:17 -0400

    4:24 commit

--- server.js
+++ server.js
@@ -40,6 +40,15 @@ app.get("/fruits", async  (req,res) => {
     res.render("fruits/index.ejs", {fruits: allFruits})
 })
 
+app.get("/fruits/:fruitId", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.FruitId)
+  res.send(`This route renders the show page for fruit id: ${req.params.fruitId}!`
+  );
+  res.render("fruits/show.ejs", { fruit: foundFruit})
+
+});
+
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
