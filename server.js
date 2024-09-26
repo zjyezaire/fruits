7a44f62d5b839ac38d077daed0e5056aadd0a94c
zjyezaire <zaire.elleby@gmail.com>
2024-09-26 13:57:43 -0400

    safely away from vim

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+
+})
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
