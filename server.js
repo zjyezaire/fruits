2a60de2579ca7ba12919b8cf2ce8ba558782c260
zjyezaire <zaire.elleby@gmail.com>
2024-09-26 14:54:57 -0400

    fruits added to database

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
+const Fruit = require("./models/fruit.js")
 mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.on("connected", () => {
@@ -21,6 +23,18 @@ app.get("/fruits/new", (req, res) => {
     res.render("fruits/new.ejs");
 
 })
+
+app.post("/fruits", async (req,res) => {
+    if(req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new")
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
